f99b9c9056721be1ab710d1e8370f76abd3d9af5
PrameshNepal7 <prameshnepal7@gmail.com>
2022-10-10 10:24:08 +0545

    minor changes

---
+++ data.py
@@ -0,0 +1,17 @@
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+print ("first number is : ",num1)
+print ("second number is : ",num2)
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+pow = num1 ** num2
+rem = num1 % num2
+print ("addition of two number is ", add)
+print ("substraction of two number is ", sub)
+print ("multiplication of two number is ", mul)
+print ("division of two number is ", div)
+print ("power of two number is ", pow)
+print ("reminder of two number is ", rem)
+print("github")
